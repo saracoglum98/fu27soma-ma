044011568189b376b88ac5973110dae12b516951
saracoglum98 <70754740+saracoglum98@users.noreply.github.com>
2025-08-20 18:46:15 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -9,10 +9,10 @@ curl -fsSL get.docker.com | bash
 
 # uv package manager
 curl -LsSf https://astral.sh/uv/install.sh | sh
-source $HOME/.local/bin/env
 
 # LM studio
-wget https://installers.lmstudio.ai/linux/x64/0.3.20-4/LM-Studio-0.3.20-4-x64.AppImage -O lms.AppImage
+mkdir exec
+wget https://installers.lmstudio.ai/linux/x64/0.3.20-4/LM-Studio-0.3.20-4-x64.AppImage -O exec/lms.AppImage
 chmod +x lms.AppImage
 nohup xvfb-run ./lms.AppImage --no-sandbox > /dev/null 2>&1 < /dev/null &
 echo -e "y\ny\ny" | npx --yes lmstudio install-cli
