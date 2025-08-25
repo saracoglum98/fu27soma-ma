23cd11b6fcb1ca03b5a532aca571d9b58b7a841b
saracoglum98 <70754740+saracoglum98@users.noreply.github.com>
2025-08-25 11:12:14 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -18,12 +18,22 @@ curl -LsSf https://astral.sh/uv/install.sh | sh
 mkdir exec
 wget https://installers.lmstudio.ai/linux/x64/0.3.20-4/LM-Studio-0.3.20-4-x64.AppImage -O exec/lms.AppImage
 chmod +x exec/lms.AppImage
-#./lms.AppImage --appimage-extract
-#nohup xvfb-run -a ./exec/lms.AppImage --no-sandbox > /dev/null 2>&1 < /dev/null &
+cd exec
+./lms.AppImage --appimage-extract
+mv squashfs-root/ lms/
+rm -rf lms.AppImage
+chmod +x lms/lm-studio
+nohup xvfb-run -a ./lms/lm-studio --no-sandbox > /dev/null 2>&1 < /dev/null &
+echo -e "y\ny\ny" | npx --yes lmstudio install-cli
+export PATH="$PATH:/root/.lmstudio/bin"
+lms server start
+lms server stop
+sed -i 's/127.0.0.1/0.0.0.0/g' /root/.lmstudio/.internal/http-server-config.json
+sed -i 's/"enableLocalService": false/"enableLocalService": true/g' /root/.config/LM Studio/settings.json
+#sed -i 's/old_string/new_string/g' filename
 # change in place - config/LM Studio/settings.json
 # change in place - /.lmstudio/.internal/http-server
 #sleep 10
-echo -e "y\ny\ny" | npx --yes lmstudio install-cli
 
 # Final
 echo 'llm-se() { ~/fu27soma-ma/manage.sh "$@" ;}' >> ~/.zshrc
