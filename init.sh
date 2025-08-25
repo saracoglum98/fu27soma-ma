4be19bc052bda86b088b2a16da19d7aaa0c9c261
saracoglum98 <mert@saracoglu.com.tr>
2025-08-25 08:00:40 +0000

    checkout

--- init.sh
+++ init.sh
@@ -19,8 +19,9 @@ mkdir exec
 wget https://installers.lmstudio.ai/linux/x64/0.3.20-4/LM-Studio-0.3.20-4-x64.AppImage -O exec/lms.AppImage
 chmod +x exec/lms.AppImage
 #./lms.AppImage --appimage-extract
-#
 #nohup xvfb-run -a ./exec/lms.AppImage --no-sandbox > /dev/null 2>&1 < /dev/null &
+# change in place - config/LM Studio/settings.json
+# change in place - /.lmstudio/.internal/http-server
 #sleep 10
 echo -e "y\ny\ny" | npx --yes lmstudio install-cli
 
